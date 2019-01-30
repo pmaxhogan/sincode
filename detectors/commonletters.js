84215362ab173fe97fd4c7ffd7d8fb8672e01122
programmer5000-com <programmer5000@programmer5000.com>
2019-01-30 08:55:42 -0500

    Fix detector

--- detectors/commonletters.js
+++ detectors/commonletters.js
@@ -1,1 +1,26 @@
+// most common letters
 const commonletters = "etaoinsrhldcumfpgwybvkxjqz";
+
+module.exports = (text) => {
+	// get the letters from text
+	const letters = text.toLowerCase().replace(/[^a-z]/g, "").split("");
+	console.log(letters);
+
+	// eg. {"e": 5, "t": 3, "l": 1}
+	const histogram = {};
+	letters.forEach(letter => {
+		// increment / create the count of this letter
+		if(histogram[letter]){
+			histogram[letter] ++;
+		}else{
+			histogram[letter] = 1;
+		}
+	});
+
+	console.log(Object.entries(histogram).sort((a, b) => {
+		// eg. a is ["e", 5] and b is ["t", 3]
+		const firstSort = b[1] - a[1];
+		if(firstSort !== 0) return firstSort;
+		return a[0].charCodeAt(0) - b[0].charCodeAt(0);
+	}).reduce((acc, [letter]) => acc + letter, ""));
+};
--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const path = require("path");
 
 const text = fs.readFileSync(path.join(__dirname, "text.txt")).toString();
 
-let mutations = [];
+let mutations = [text];
 fs.readdirSync(path.join(__dirname, "mutations")).forEach(mutationName => {
 	// load a module from the mutations folder and add its output
-	mutations = mutations.concat(
-		require(
-			path.join(__dirname, "mutations/" + mutationName)
-		)(text)
-	);
+	// mutations = mutations.concat(
+	// 	require(
+	// 		path.join(__dirname, "mutations/" + mutationName)
+	// 	)(text)
+	// );
 });
 
-console.log(mutations);
+mutations.forEach(mutation => {
+	fs.readdirSync(path.join(__dirname, "detectors")).forEach(detectorName => {
+		// load a module from the mutations folder and add its output
+		console.log(
+			require(
+				path.join(__dirname, "detectors/" + detectorName)
+			)(mutation)
+		);
+	});
+});
