723423fba514241396dc8dd546d50fec45fefa6b
realprogrammer5000 <me@programmer5000.com>
2020-04-12 23:56:33 -0400

    Add rot test

--- test.js
+++ test.js
@@ -19,3 +19,10 @@ const mut2 = mutations.map(mutation => knownWordsDetector(mutation.text, {fast: 
 console.log(Number(process.hrtime.bigint() - start) / NS_PER_SEC + "s");
 
 console.log(JSON.stringify(mut1) === JSON.stringify(mut2));
+
+const rot = require("./mutations/3-rot");
+const rotM = rot("Bzl jhbapvu dolu thrpun bzl vm Lewlyptluahs mlhabylz, whyapjbshysf dpaopu tvkbslz. Luk bzlyz thf uva il hdhyl aoha lewlyptluahs mlhabylz hyl ilpun bzlk. Ibnz vy ilohcpvy johunlz thf zbywypzl luk bzlyz dolu Lewlyptluahs HWP tvkpmpjhapvuz vjjby. Av hcvpk zbywypzlz, bzl vm hu Lewlyptluahs mlhabyl thf ullk h jvtthuk-spul mshn. Lewlyptluahs mlhabylz thf hszv ltpa h dhyupun.\n", {
+    keepEveryMax: 30,
+    deleteEveryMax: 15
+});
+console.log(rotM);
